3966857a58000ee59f556cbf0601d9ef5ca9212e
Joseph Horton <josephhorton95@gmail.com>
2022-03-12 23:05:56 +0000

    Move components into own files

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,9 @@ import {
 } from './lib/points-worker';
 
 import './App.css';
+import Options from './Components/Options';
+import Loader from './Components/Loader';
+import { Centered, TextLink } from './Components/Layout';
 
 interface MandelBulbProps {
   mandel: Float32Array;
@@ -207,11 +210,13 @@ function App() {
             {'< / >'}
           </TextLink>
         </div>
+
         <div className="grid-row-canvas">
           <div className="canvas-container">
             {status === 'LOADED' && points && <Scene points={points} />}
           </div>
         </div>
+
         <div className="grid-row-text">
           <Options
             onSave={regenerateMandelbulb}
@@ -249,145 +254,3 @@ function App() {
 }
 
 export default App;
-
-interface OptionsProps {
-  onSave: (m: MandelbulbParams) => void;
-  isGenerating: boolean;
-}
-
-const Options = ({ onSave, isGenerating }: OptionsProps) => {
-  const nPowerOptions = [2, 4, 6, 8, 12, 16, 32];
-  const maxIterationsOptions = [10, 20, 32, 64, 128, 256];
-  const dimOptions = [16, 24, 32, 64, 128, 256];
-  const [value, setValue] = useState<MandelbulbParams>({
-    nPower: 8,
-    maxIterations: 80,
-    dim: 64,
-  });
-
-  const handleOnSave = () => {
-    console.log('call on save with values', value);
-    onSave(value);
-  };
-
-  return (
-    <div className="options-grid">
-      <div className="item">
-        <label>N Power</label>
-        <select
-          value={value.nPower}
-          onChange={(e) =>
-            setValue({ ...value, nPower: parseInt(e.target.value) })
-          }
-        >
-          {nPowerOptions.map((opt) => (
-            <option key={opt} value={opt}>
-              {opt}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="item">
-        <label>Max Iterations</label>
-        <select
-          disabled={false}
-          value={value.maxIterations}
-          onChange={(e) =>
-            setValue({ ...value, maxIterations: parseInt(e.target.value) })
-          }
-        >
-          {maxIterationsOptions.map((opt) => (
-            <option key={opt} value={opt}>
-              {opt}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="item">
-        <label>Dimension</label>
-        <select
-          disabled={false}
-          value={value.dim}
-          onChange={(e) =>
-            setValue({ ...value, dim: parseInt(e.target.value) })
-          }
-        >
-          {dimOptions.map((opt) => (
-            <option key={opt} value={opt}>
-              {opt}
-            </option>
-          ))}
-        </select>
-      </div>
-      <button
-        disabled={isGenerating}
-        onClick={handleOnSave}
-        style={{ opacity: isGenerating ? 0.4 : 1.0 }}
-      >
-        update
-      </button>
-    </div>
-  );
-};
-
-const TextLink = ({
-  url,
-  children,
-}: {
-  url: string;
-  children: React.ReactChild;
-}) => (
-  <a href={url} target="_blank" rel="noreferrer">
-    {children}
-  </a>
-);
-
-const Centered = ({ children }: { children: React.ReactChild }) => (
-  <div className="loader-outer">
-    <div className="loader-inner">{children}</div>
-  </div>
-);
-
-const randPoints: number[] = Array.from({ length: 20 }, (_, i) => i);
-
-const Loader = () => {
-  return (
-    <div className="loader-outer">
-      <div className="loader-inner">
-        <p>generating points</p>
-        {randPoints.map((point) => (
-          <LoaderPoint key={point} index={point} />
-        ))}
-      </div>
-    </div>
-  );
-};
-
-const newPos = () => ({
-  x: (Math.random() - 0.5) * 200,
-  y: (Math.random() - 0.5) * 200,
-});
-
-const LoaderPoint = ({ index }: { index: number }) => {
-  const [pos, setPos] = useState(() => newPos());
-  const { x, y, opacity } = useSpring({
-    x: pos.x,
-    y: pos.y,
-    opacity: Math.random(),
-    from: { x: pos.y, y: pos.x, opacity: Math.random() },
-    onRest: () => setPos(() => newPos()),
-    config: config.molasses,
-  });
-
-  return (
-    <animated.div
-      className="loader-point"
-      key={index}
-      style={{
-        x,
-        y,
-        opacity,
-      }}
-    />
-  );
-};
