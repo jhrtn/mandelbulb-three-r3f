4dd781f757e0d58e9d9bc9771d328892a9d0e3f9
Joseph Horton <josephhorton95@gmail.com>
2022-03-12 14:23:04 +0000

    Layout and links

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import { useRef, Suspense, useMemo } from 'react';
+import React, { useRef, Suspense, useState, useEffect } from 'react';
 import * as THREE from 'three';
-import { useTexture, OrbitControls, Stats } from '@react-three/drei';
+import { useTexture, OrbitControls } from '@react-three/drei';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { EffectComposer, Bloom } from '@react-three/postprocessing';
 
 import './App.css';
-
-const particleTextures = [
-  'textures/1.png',
-  'textures/2.png',
-  'textures/3.png',
-  'textures/4.png',
-  'textures/5.png',
-  'textures/6.png',
-  'textures/7.png',
-  'textures/8.png',
-  'textures/9.png',
-  'textures/10.png',
-  'textures/11.png',
-  'textures/12.png',
-  'textures/13.png',
-];
 
 interface Spherical {
   r: number;
@@ -42,10 +26,7 @@ const toSpherical = (x: number, y: number, z: number): Spherical => {
   return { r, theta, phi };
 };
 
-const createMandel = () => {
-  console.log('recalculating...');
-  // z = z^n + c <- triplex number
-
+const createMandel = (): Promise<Float32Array> => {
   const { mapLinear } = THREE.MathUtils;
   const { pow, sin, cos } = Math;
 
@@ -54,77 +35,69 @@ const createMandel = () => {
   const dim = 128;
 
   const points: number[] = [];
-  for (let i = 0; i < dim; i++) {
-    for (let j = 0; j < dim; j++) {
-      let isEdge = false;
-      for (let k = 0; k < dim; k++) {
-        const x = mapLinear(i, 0, dim, -posOffset, posOffset);
-        const y = mapLinear(j, 0, dim, -posOffset, posOffset);
-        const z = mapLinear(k, 0, dim, -posOffset, posOffset);
 
-        const zeta: Coord = { x: 0.0, y: 0.0, z: 0.0 };
+  const pointsPromise = new Promise<Float32Array>((resolve) => {
+    for (let i = 0; i < dim; i++) {
+      for (let j = 0; j < dim; j++) {
+        let isEdge = false;
+        for (let k = 0; k < dim; k++) {
+          const x = mapLinear(i, 0, dim, -posOffset, posOffset);
+          const y = mapLinear(j, 0, dim, -posOffset, posOffset);
+          const z = mapLinear(k, 0, dim, -posOffset, posOffset);
 
-        const n = 8;
-        const maxIterations = 20;
-        let iteration = 0;
+          const zeta: Coord = { x: 0.0, y: 0.0, z: 0.0 };
 
-        while (true) {
-          const { r, theta, phi } = toSpherical(zeta.x, zeta.y, zeta.z);
+          const n = 8;
+          const maxIterations = 20;
+          let iteration = 0;
 
-          const newx = pow(r, n) * sin(theta * n) * cos(phi * n);
-          const newy = pow(r, n) * sin(theta * n) * sin(phi * n);
-          const newz = pow(r, n) * cos(theta * n);
+          while (true) {
+            const { r, theta, phi } = toSpherical(zeta.x, zeta.y, zeta.z);
 
-          zeta.x = newx + x;
-          zeta.y = newy + y;
-          zeta.z = newz + z;
+            const newx = pow(r, n) * sin(theta * n) * cos(phi * n);
+            const newy = pow(r, n) * sin(theta * n) * sin(phi * n);
+            const newz = pow(r, n) * cos(theta * n);
 
-          // console.log(zeta);
+            zeta.x = newx + x;
+            zeta.y = newy + y;
+            zeta.z = newz + z;
 
-          iteration++;
+            iteration++;
 
-          if (r > 2) {
-            if (isEdge) {
-              isEdge = false;
+            if (r > 2) {
+              if (isEdge) {
+                isEdge = false;
+              }
+              break;
             }
-            break;
-          }
 
-          if (iteration > maxIterations) {
-            if (!isEdge) {
-              isEdge = true;
-              points.push(zeta.x, zeta.y, zeta.z);
+            if (iteration > maxIterations) {
+              if (!isEdge) {
+                isEdge = true;
+                points.push(zeta.x, zeta.y, zeta.z);
+              }
+              break;
             }
-            break;
           }
         }
       }
     }
-  }
 
-  return new Float32Array(points);
+    resolve(new Float32Array(points));
+  });
+
+  return pointsPromise;
 };
 
 interface MandelBulbProps {
   mandel: Float32Array;
-  needsUpdate: boolean;
 }
 
-const Mandelbulb = ({ mandel, needsUpdate }: MandelBulbProps) => {
+const Mandelbulb = ({ mandel }: MandelBulbProps) => {
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const pointsRef = useRef<any>(null!); // eslint-disable-line @typescript-eslint/no-explicit-any
 
-  const particleTexture = useTexture(particleTextures[0]);
-
-  useFrame(() => {
-    if (pointsRef.current && needsUpdate) {
-      console.log('do something...');
-      needsUpdate = false;
-    }
-  });
-
-  console.log('re-render');
-
+  const particleTexture = useTexture('textures/1.png');
   useFrame((state) => {
     if (pointsRef.current)
       pointsRef.current.rotation.y = state.clock.getElapsedTime() * 0.1;
@@ -153,50 +126,88 @@ const Mandelbulb = ({ mandel, needsUpdate }: MandelBulbProps) => {
   );
 };
 
-function App() {
-  const mandelPoints = useMemo(() => createMandel(), []);
+const Scene = ({ points }: { points: Float32Array }) => {
+  return (
+    <Canvas
+      // dpr={[1, 2]}
+      orthographic
+      camera={{ zoom: 140, position: [0, 0, 100] }}
+    >
+      <ambientLight />
+      <OrbitControls
+        makeDefault
+        enableZoom={true}
+        enablePan={false}
+        minZoom={140}
+        maxZoom={400}
+      />
+      <Suspense fallback={null}>
+        {points && <Mandelbulb mandel={points} />}
+      </Suspense>
+      {/* <Stats /> */}
+      <EffectComposer>
+        <Bloom
+          luminanceThreshold={0.4}
+          luminanceSmoothing={0.9}
+          height={window.innerHeight}
+          width={window.innerWidth}
+        />
+      </EffectComposer>
+    </Canvas>
+  );
+};
 
-  const state = useRef({
-    points: mandelPoints,
-    needsUpdate: false,
-  });
+function App() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [loading, setLoading] = useState(true);
+  const [points, setPoints] = useState<Float32Array | null>();
+  useEffect(() => {
+    createMandel().then((result) => {
+      setPoints(result);
+      setLoading(false);
+    });
+  }, []);
 
   return (
-    <div className="container">
-      <Canvas
-        style={{ height: '100vh' }}
-        // dpr={[1, 2]}
-        orthographic
-        camera={{ zoom: 140, position: [0, 0, 100] }}
-      >
-        <color attach="background" args={['black']} />
-        <ambientLight />
-
-        <OrbitControls
-          makeDefault
-          enableZoom={true}
-          enablePan={false}
-          minZoom={140}
-          maxZoom={400}
-        />
-        <Suspense fallback={null}>
-          <Mandelbulb
-            mandel={state.current.points}
-            needsUpdate={state.current.needsUpdate}
-          />
-        </Suspense>
-        <Stats />
-        <EffectComposer>
-          <Bloom
-            luminanceThreshold={0.4}
-            luminanceSmoothing={0.9}
-            height={window.innerHeight}
-            width={window.innerWidth}
-          />
-        </EffectComposer>
-      </Canvas>
-    </div>
+    <>
+      {/* {true && (
+        <div className="loader-overylay">
+          <p>generating mandelbulb points...</p>
+        </div>
+      )} */}
+      <div className="grid-container">
+        <div className="grid-row-text">
+          <p>Mandelbulb Point Cloud // Three.js/R3F</p>
+          <TextLink url="https://github.com/jhrtn/mandelbulb-three-r3f">
+            {'< / >'}
+          </TextLink>
+        </div>
+        <div className="grid-row-canvas">
+          <div className="canvas-container">
+            {points && <Scene points={points} />}
+          </div>
+        </div>
+        <div className="grid-row-text">
+          <TextLink url="https://thecodingtrain.com/CodingChallenges/168-mandelbulb.html">
+            Ported from Daniel Shiffman&apos;s Processing code
+          </TextLink>
+          <TextLink url="https://hort.onl">by Joseph Horton</TextLink>
+        </div>
+      </div>
+    </>
   );
 }
 
 export default App;
+
+const TextLink = ({
+  url,
+  children,
+}: {
+  url: string;
+  children: React.ReactChild;
+}) => (
+  <a href={url} target="_blank" rel="noreferrer">
+    {children}
+  </a>
+);
